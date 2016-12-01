f8a4c44e45d5bd7a6120a1643a1ac2f98005afe9
francis <chicano@lcc.uma.es>
2016-12-01 19:57:12 +0100

    Aplicación Web Spring

---
+++ src/main/java/es/uma/informatica/springmvc/primerawebapp/controller/HomeController.java
@@ -0,0 +1,17 @@
+package es.uma.informatica.springmvc.primerawebapp.controller;
+
+import org.springframework.stereotype.Controller; 
+import org.springframework.ui.Model; 
+import org.springframework.web.bind.annotation.RequestMapping; 
+
+@Controller 
+public class HomeController { 
+
+  @RequestMapping("/") 
+  public String welcome(Model model) { 
+    model.addAttribute("saludo", "¡Bienvenidos a Supermercados Albosque!"); 
+    model.addAttribute("eslogan", "Compre Albosque, compre al mejor"); 
+
+    return "welcome"; 
+  } 
+} 
