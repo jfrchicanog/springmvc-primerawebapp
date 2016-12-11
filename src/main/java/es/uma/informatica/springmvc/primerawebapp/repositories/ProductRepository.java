212b70531b04d0682420da6bf4c82318a9af4d8d
francis <chicano@lcc.uma.es>
2016-12-11 01:02:31 +0100

    Refactorización

--- src/main/java/es/uma/informatica/springmvc/primerawebapp/repositories/ProductRepository.java
+++ src/main/java/es/uma/informatica/springmvc/primerawebapp/repositories/ProductRepository.java
@@ -1,7 +1,6 @@
 package es.uma.informatica.springmvc.primerawebapp.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-
 import es.uma.informatica.springmvc.primerawebapp.domain.Producto;
 
 public interface ProductRepository extends CrudRepository<Producto,Long>{
